ff2e9f6ac189fe432f249ab99376f022da869b22
Nexs12 <152890031+Nexs12@users.noreply.github.com>
2024-05-13 20:29:55 +0000

    Deploying to gh-pages from @ Nexs12/goit-js-hw-09@7798d23a8404b536a2a53a2189460c64dd258164 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const o=document.querySelector(".feedback-form");o.addEventListener("input",r);const t={email:"",message:""};function r(e){const a=e.target.name;t[a]=e.target.value,localStorage.setItem("feedback-form-state",JSON.stringify(t))}
+//# sourceMappingURL=commonHelpers2.js.map
