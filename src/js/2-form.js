a4c7dea9bc7a7fae1ba385e9dfef2ce88678fe1b
Nexs12 <152890031+Nexs12@users.noreply.github.com>
2024-05-14 21:00:48 +0300

    5

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -6,13 +6,20 @@ form.addEventListener('input', handleInput)
 
 const formData = {
     email: '',
-    message: ''
+    message: '',
+    clear() {
+      this.email = '';
+      this.message = '';
+    },
 }
+
+document.addEventListener('DOMContentLoaded', populateText);
 
 function handleInput(event) {
     const key = event.target.name
+    const value = event.target.value.trim();
     
-    formData[key] = event.target.value;
+    formData[key] = value;
 
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
@@ -29,3 +36,21 @@ function populateText() {
     email.value = data.email;
     message.value = data.message;
 }
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+
+  
+    if (formData.email === '' || formData.message === '') {
+        return alert('Please fill in all fields');
+    }
+
+    console.log(formData);
+
+    localStorage.removeItem('feedback-form-state');
+    formData.clear();
+    form.reset();
+};
